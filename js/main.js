45773b9ef6a3022b70aaba47c4e3acc5021ce853
oluwakeye-john <bjohnoluwakeye@gmail.com>
2021-05-22 19:07:35 +0100

    vcard scroll in

--- js/main.js
+++ js/main.js
@@ -3,14 +3,35 @@
     const hamburgerButton = document.querySelector("#mobile-hamburger");
     const menuList = document.querySelector(".header__left__list");
 
+    const openClass = "is-hamburger-open";
+
     hamburgerButton.addEventListener("click", () => {
       //show-list
-      if (menuList.classList.contains("is-open")) {
-        menuList.classList.remove("is-open");
+      if (menuList.classList.contains(openClass)) {
+        menuList.classList.remove(openClass);
       } else {
-        menuList.classList.add("is-open");
+        menuList.classList.add(openClass);
       }
     });
   }
+
+  function handleVCardScroll() {
+    const vcard = document.querySelector("#desktop-vcard");
+    const isOpenClass = "show-vcard";
+
+    const visibleThreshold = 330;
+
+    window.document.addEventListener("scroll", () => {
+      if (window.scrollY > visibleThreshold) {
+        console.log("open");
+        vcard.classList.add(isOpenClass);
+      } else {
+        console.log("cloase");
+        vcard.classList.remove(isOpenClass);
+      }
+    });
+  }
+
   handleHamburger();
+  handleVCardScroll();
 })();
